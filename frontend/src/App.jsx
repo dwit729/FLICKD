c0b8426b6dc4c24a8c99de7ecabc42d8ea479054
Adrian Pueblo <adrian.pueblo27@gmail.com>
2024-10-23 00:27:05 +0800

    Added Template for Carousel and Other Design.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 import LogInPage from './pages/LogInPage';
 import Navbar from './components/NavBar';
+import HomePage from './pages/HomePage';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
       <Navbar/>
         <Routes>
+          <Route path='/Home' element={<HomePage/>}></Route>
           <Route path='/Login' element={<LogInPage/>}></Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
-import { Flex, Layout } from 'antd';
+import { Carousel, Flex, Layout } from 'antd';
+
+import '../css/Carousel.css';
+
 const { Header, Footer, Sider, Content } = Layout;
-
-
 
 const HomePage = () => {
   return (
-      <div className="content-container">
+      <>
+        <div className="content-container">
           <div className="carousel-container">
-
+            <Carousel arrows autoplay={true} autoplaySpeed={2000} infinite={true}>
+              <div>
+                <h3 className="carousel-style">1</h3>
+              </div>
+              <div>
+                <h3 className="carousel-style">2</h3>
+              </div>
+              <div>
+                <h3 className="carousel-style">3</h3>
+              </div>
+              <div>
+                <h3 className="carousel-style">4</h3>
+              </div>
+            </Carousel>
           </div>
-          <div className="info-container"></div>
-      </div>
+          <div className="info-container">
+              <h1 className="title-heading">Watch, Feel, Review, and Share! Experience FLiCKD!</h1>
+              <div className="content-segment">
+                  <span>
+                    <p>Keep track of every film you’ve ever watched (or just start from the day you join)</p>
+                  </span>
+              </div>
+              <div className="content-segment">
+                  <span>
+                    <p>Show some love for your favorite films, lists and reviews with a “like”</p>
+                  </span>
+              </div>
+              <div className="content-segment">
+                  <span>
+                    <p>Rate each film on a five-star scale (with halves) to record and share your reaction</p>
+                  </span>
+              </div>
+          </div>
+        </div>
+      </>
   )
 }
 
-export default HomePage
+export default HomePage;
