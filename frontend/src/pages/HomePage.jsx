29572b1b3cbcf148944dc3233ba3a23b74be96ae
Adrian Pueblo <adrian.pueblo27@gmail.com>
2024-10-24 16:22:02 +0800

    Added dynamic info

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,8 +10,49 @@ import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
 
 const { Header, Footer, Sider, Content } = Layout;
 
+const movieList = [
+  {
+    title: "Spider Man: No Way Home",
+    year: "2022",
+    banner: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/Spider-Man-No-Way-Home-All-Three-Spider-Men-Together.jpg",
+    cover: "https://images.moviesanywhere.com/5a7e82e7749d6dd8e447fcfeec50c064/55462afc-41ad-4322-ad46-1caca29fcc36.jpg",
+    favoriteCount: "11.1K"
+  },
+  {
+    title: "Sherlock",
+    year: "2010",
+    banner: "https://player.bbc.com/static/posters/1810556/original/1585653092-069c50d32152399ae9f0d81ca5df32e35a7695c9.jpg",
+    cover: "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_.jpg",
+    favoriteCount: "11.1K"
+  },
+  {
+    title: "Dead Sea",
+    year: "2024",
+    banner: "https://www.highonfilms.com/wp-content/uploads/2024/08/Dead-Sea2.jpg",
+    cover: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26900448_p_v13_ab.jpg",
+    favoriteCount: "11.1K"
+  },
+  {
+    title: "Bad Boys: Ride or Die",
+    year: "2024",
+    banner: "https://images.bauerhosting.com/empire/2024/06/bad-boys-2.png?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80",
+    cover: "https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg",
+    favoriteCount: "11.1K"
+  }
+];
+
 const HomePage = () => {
+
+  const [sliderCount, setSliderCount] = useState(0);
+  const [currentMovie, setcurrentMovie] = useState(movieList[0]);
+
+  
+
   function determineArrow(type) {
+    setSliderCount(type === "right" ? sliderCount + 1 : sliderCount  - 1);
+    setcurrentMovie(movieList[sliderCount])
+    
+
     return (
       <FontAwesomeIcon
         size="3x"
@@ -25,43 +66,28 @@ const HomePage = () => {
         <div className="carousel-container">
           <Carousel
             arrows
-            autoplay={true}
+            autoplay={false}
             autoplaySpeed={2000}
             infinite={true}
             nextArrow={determineArrow("right")}
             prevArrow={determineArrow("left")}
           >
-            <div className="carousel-style">
-              <img
-                src="https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/Spider-Man-No-Way-Home-All-Three-Spider-Men-Together.jpg"
-                alt="item1"
-              />
-            </div>
-            <div className="carousel-style">
-              <img
-                src="https://player.bbc.com/static/posters/1810556/original/1585653092-069c50d32152399ae9f0d81ca5df32e35a7695c9.jpg"
-                alt="item2"
-              />
-            </div>
-            <div className="carousel-style">
-              <img
-                src="https://www.highonfilms.com/wp-content/uploads/2024/08/Dead-Sea2.jpg"
-                alt="item3"
-              />
-            </div>
-            <div className="carousel-style">
-              <img
-                src="https://images.bauerhosting.com/empire/2024/06/bad-boys-2.png?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80"
-                alt="item4"
-              />
-            </div>
+
+            {movieList.map((i) => {
+              return (
+                <div className="carousel-style">
+                  <img src={i.banner} alt="item1" />
+                </div>
+              );
+            })}	
+
           </Carousel>
 
           <div className="cover-section">
             <div className="cover-template">
               <img
                 src={
-                  "https://images.moviesanywhere.com/5a7e82e7749d6dd8e447fcfeec50c064/55462afc-41ad-4322-ad46-1caca29fcc36.jpg"
+                  currentMovie.cover
                 }
                 className="cover-img"
                 alt="img-cover"
@@ -69,13 +95,13 @@ const HomePage = () => {
             </div>
 
             <div className="cover-info">
-              <h1 className="cover-text">Spider-Man: No Way Home</h1>
+              <h1 className="cover-text">{currentMovie.title}</h1>
               <div className="cover-year">
-                <h1 className="cover-text">2024</h1>
+                <h1 className="cover-text">{currentMovie.year}</h1>
               </div>
               <div className="cover-hearts">
                 <FontAwesomeIcon icon={faHeart} size="2x"/>
-                <h1 className="cover-hearts-txt">11.1k</h1>
+                <h1 className="cover-hearts-txt">{currentMovie.favoriteCount}</h1>
               </div>
             </div>
           </div>
