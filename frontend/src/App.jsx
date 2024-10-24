dc90156a92687f880717332964a080435ae335e6
Adrian Pueblo <adrian.pueblo27@gmail.com>
2024-10-24 22:54:35 +0800

    Additions to profile.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import AccountSettings from './pages/AccountSettings';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/Home' element={<HomePage/>}></Route>
           <Route path='/Login' element={<LogInPage/>}></Route>
           <Route path='/SignUp' element={<SignUpPage/>}></Route>
+          <Route path='/Profile' element={<ProfilePage/>}></Route>
           <Route path='/Account_Settings' element={<AccountSettings/>}></Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
     <div>
+      <div className="banner"></div>
+      <div className="profile-section">
+        <div className="img-template">
+          <img
+            src="https://static.thenounproject.com/png/65476-200.png"
+            alt="user_icon"
+          />
+        </div>
+        <div className="profile-info">
+          <h1 className="title">Username</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
