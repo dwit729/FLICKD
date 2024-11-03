a84807e1ea77870d459f46945e07b13b94347845
Adrian Pueblo <adrian.pueblo27@gmail.com>
2024-11-04 00:18:43 +0800

    Added edit button on Account Settings.

--- frontend/src/pages/AccountSettings.jsx
+++ frontend/src/pages/AccountSettings.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import "../css/Button.css";
+
+import React, {useState} from "react";
 import { Input } from "antd";
-import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { 
+  EyeInvisibleOutlined, 
+  EyeTwoTone, 
+  EditOutlined, 
+  CloseCircleOutlined, 
+  SaveOutlined } from "@ant-design/icons";
 
 const AccountSettings = () => {
+  const [access, setAccess] = useState(true)
+  const [edit, setEdit] = useState(false)
+
+  const handleAccess = () => {
+    if(access) {setAccess(false); setEdit(true)} 
+    else {setAccess(true); setEdit(false)}
+  }
+
+  const handleAccessButton = () => {
+    return access ? 
+    <button onClick={() => handleAccess()} className="button-primary"> 
+      Edit &nbsp; 
+    <EditOutlined /> 
+    </button> :
+    <button onClick={() => handleAccess()} className="button-complimentary"> 
+      Cancel &nbsp; 
+      <CloseCircleOutlined /> 
+    </button>
+  }
+
+  const showSave = () => {
+    return edit ? <button className="button-primary"> 
+    Save &nbsp; 
+    <SaveOutlined />
+  </button> : "";
+  }
+
   return (
     <div className="content-section">
       <div className="details-container">
@@ -10,12 +44,13 @@ const AccountSettings = () => {
 
         <div className="text-field">
           <h3 className="subtitle">Username</h3>
-          <Input placeholder="oWo111" />
+          <Input placeholder="oWo111" disabled={access} />
         </div>
 
         <div className="text-field">
           <h3 className="subtitle">Password</h3>
           <Input.Password
+            disabled={access}
             placeholder="123password"
             iconRender={(visible) =>
               visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
@@ -23,7 +58,10 @@ const AccountSettings = () => {
           />
         </div>
 
-        <button className="button-compliment">Get Started - It's free!</button>
+        <div className="button-container">
+          {handleAccessButton()}
+          {showSave()}
+        </div>
       </div>
     </div>
   );
